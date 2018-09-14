a605a226975bcf2f9923db9e3823d3fc4547eddd
Kenneth Maguire <kennethmaguire@Kenneths-MBP.domain>
2018-09-14 14:19:34 -0600

    added a bunch of stuf to test js that needs testing. Added spans

--- public_html/scripts/main.js
+++ public_html/scripts/main.js
@@ -4,4 +4,46 @@
  * and open the template in the editor.
  */
 
-
+function validateForm()
+{
+    //part1 Daniel
+    
+    
+    //part2 Daniel
+    
+    
+    
+    //part3 Kenn
+    function validateForm3()
+    {
+        var valid = true;
+        var formInputs = document.getElementsByClassName("FormInput");
+        var i;
+        var testP = document.getElementById("test");
+        for(i = 0; i < formInputs.length; i++)
+        {
+            var searchVal;
+            if(formInputs[i].type == "text"){           //used this input statement for testing
+            
+            //use regex to check for '
+                    
+            valid = false;
+          
+            testP.innerText = "testing worked";
+            }
+            
+            
+        }
+        
+        return valid;
+        
+    }
+    return validateForm3();
+    
+    
+    
+    //part4 Kenn
+    
+    
+    
+}
