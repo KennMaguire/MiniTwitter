2414be6b2ee9ad8f530ed1716c472c201639657c
Kenneth Maguire <kennethmaguire@Kenneths-MBP.domain>
2018-09-15 15:38:41 -0600

    finished check 3, added function to make span err visible

--- public_html/scripts/main.js
+++ public_html/scripts/main.js
@@ -4,6 +4,13 @@
  * and open the template in the editor.
  */
 
+function makeSpanErrorVisible(_inputName)               //This function makes span errors visible by using the name of the input, append _error to it, and 
+{                                                       //setting the spanError to visible
+     var spanErrorName = _inputName + "_error";
+     var spanError = document.getElementById(spanErrorName);
+     spanError.className = "spanVisible";
+}
+
 function validateForm()
 {
     //part1 Daniel
@@ -16,24 +23,24 @@ function validateForm()
     //part3 Kenn
     function validateForm3()
     {
-        var valid = true;
-        var formInputs = document.getElementsByClassName("FormInput");
-        var i;
-        var errorDiv = document.getElementById("errorMessage");
+        var valid = true;                                                                                    //default value true before test loop
+        var formInputs = document.getElementsByClassName("FormInput");                                       //get an array of all form inputs
+        var i;           //iterator                                                           
+        var errorDiv = document.getElementById("errorMessage");                                              //get errorMessage div object
         for(i = 0; i < formInputs.length; i++)
         {
-            var searchVal;
-            if(formInputs[i].name !== "securityQuestion" && formInputs[i].name !== "dateOfBirth"){           //used this input statement for testing
-            //use regex to check for '
-            
-
-            var searchStr = formInputs[i].value;
-            var n = searchStr.search("'");
-                if(n !== -1)
-                {
-                    valid = false;
-                    errorDiv.innerHTML = "<p>**INPUT HAS INVALID CHARACTERS**</p>";
-                    errorDiv.className = "visible";
+            if(formInputs[i].name !== "securityQuestion" && formInputs[i].name !== "dateOfBirth"){           //if any type of text (including email)
+            var searchStr = formInputs[i].value;                                                             
+            var n = searchStr.search("'");                                                                   //find location of ', if none n = -1
+                if(n !== -1)                                                                                 //if ' found, continue
+                {   
+                    var inputName = formInputs[i].name;
+                    makeSpanErrorVisible(inputName);                                    //call function, span error should now be visible
+                    valid = false;                                                      //make input invalid
+                    errorDiv.innerHTML = "<p>**INPUT HAS INVALID CHARACTERS**</p>";     //add html to div
+                    errorDiv.className = "divVisible";                                  //set div message to visible
+                    formInputs[i].style.backgroundColor = "yellow";                     //change background of text box to yellow
+                    
                     return valid;
                 }
             
@@ -42,7 +49,7 @@ function validateForm()
             
         }
         
-        return valid;
+        return valid;   //should only happen when valid = true, meaning no elements contain '
         
     }
     return validateForm3();
